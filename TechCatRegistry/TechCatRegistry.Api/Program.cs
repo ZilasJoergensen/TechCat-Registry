3e634a2fbca5b33b2e25f8f23675f4e4cd7b5b75
Zilas Jørgensen <zj@emd.dk>
2026-09-04 14:34:23 +0200

    Update Swashbuckle & init Swagger

--- TechCatRegistry/TechCatRegistry.Api/Program.cs
+++ TechCatRegistry/TechCatRegistry.Api/Program.cs
@@ -1,8 +1,12 @@
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -11,7 +15,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
