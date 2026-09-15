6a4db237c0d309742c916f5adcd7599c4e306c73
Zilas Jørgensen <zj@emd.dk>
2026-09-15 12:59:17 +0200

    Catalog parser tester init

---
+++ TechCatRegistry.Data/Parsing/CatalogRow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechCatRegistry.Data.Parsing;
+
+public class CatalogRow
+{
+    public string Ws { get; set; }
+    public string Technology { get; set; }
+    public string Cat { get; set; }
+    public string Par { get; set; }
+
+    public string? Unit { get; set; }
+    public string? Note { get; set; }
+    public string? Ref { get; set; }
+
+    public string Est { get; set; }
+
+    public int Year { get; set; }
+
+    public int? PriceYear { get; set; }
+
+    public string Val { get; set; }
+}
---
+++ TechCatRegistry.Tests/CatalogParserTests.cs
@@ -0,0 +1,21 @@
+﻿using ClosedXML.Excel;
+
+namespace TechCatRegistry.Tests;
+
+public class CatalogParserTests
+{
+    [Fact]
+    public void IntroArkNameCheck()
+    {
+        // https://docs.closedxml.io/
+        // https://github.com/ClosedXML/ClosedXML/tree/develop/ClosedXML.Examples
+        var TestDataPath = Path.Combine(AppContext.BaseDirectory, "TestData", "technology_data_for_el_and_dh_updated_alldatalong.xlsx");
+
+        var workbook = new XLWorkbook(TestDataPath);
+        var intro = workbook.Worksheet("Intro");
+
+        var version = intro.Cell("B3").GetString();
+
+        Assert.Contains("0019", version);
+    }
+}
